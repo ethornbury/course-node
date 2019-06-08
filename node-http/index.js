62276bbae26e1657180e5acd94b54126927bbfb9
ethornbury <thornburye@gmail.com>
2019-06-08 14:15:56 +0100

    understanding a server request

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,53 @@
 
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req, res) => {
-	console.log(req.headers);
+	console.log("Request for: " +req.url+ "by method: " +req.method);
+	if(req.method == 'GET'){
+		var fileUrl;
+		if(req.url == '/') fileUrl = '/index.html';
+		else fileUrl = req.url;
+		
+		var filePath = path.resolve('./public'+fileUrl);
+		const fileExt = path.extname(filePath);
+		if(fileExt == '.html'){
+			fs.exists(filePath, (exists) => {
+				if(!exists){
+					res.statusCode = 404;
+					res.setHeader('Content-Type', 'text/html');
+					res.end('<html><body><p>404 error, file not found '+fileUrl+' </p></body></html>');
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+				fs.createReadStream(filePath).pipe(res);
+			})
+		}else{
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/html');
+			res.end('<html><body><p>404 error: file not HTML '+fileUrl+ '</p></body></html>');
+			return;
+		}
+	}else{
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<html><body><h1>Error 404: ' + req.method + 
+              ' not supported</h1></body></html>');
+
+	}
 	
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<html><body><p>Hello World</p></body></html>');
 })
 server.listen(port, hostname, ()=>{
+	//backwards single quote allows us to make use of ${} to insert var data
 	console.log(`Server running at http:${hostname}:${port}`);
 });
+
+//https://www.getpostman.com/
+//http://127.0.0.1:3000/
+
+
