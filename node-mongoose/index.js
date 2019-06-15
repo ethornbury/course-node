b4dcca7146174bdf4fd58e8b3193172d18bf121f
Emer Thornbury <EThornbury@staff.ncirl.ie>
2019-06-15 11:04:47 +0100

    comments Schema added to Dish Schema, dish created and updated with comment

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,18 +6,43 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 	console.log("connected to mongodb server");
 	Dishes.create({ 
-		//creates and adds to db
-		name: 'pizza', 
-		description: 'test'
+		//creates and adds to db collection
+		name: 'pizza3', 
+		description: 'test3'
 	})
+	/*
 	.then((dish) =>{
 		console.log(dish);
 		return Dishes.find({}).exec(); //exec will ensure it is executed
 	})
+	*/
+	.then((dish)=>{
+		console.log(dish);
+		return Dishes.findByIdAndUpdate(dish._id, {
+			$set: {description: 'updated test'}
+		},{
+			new: true
+		}).exec();
+	})
+	.then((dish) =>{
+		console.log(dish);
+		dish.comments.push({
+			rating: 5,
+			comment: 'I\'m getting a bad feeling',
+			author: 'J Doe'
+		});
+		return dish.save();
+	}).
+	then((dish)=>{
+		console.log(dish);
+		return Dishes.deleteMany({});
+	})
+	/*
 	.then((dishes)=>{
 		console.log(dishes);
 		return Dishes.deleteMany({}); //deleteOne, deleteMany, bulkWrite
 	})
+	*/
 	.then(()=>{
 		return mongoose.connection.close();
 	})
